6ca48117126e8f2b592dcbbce962113942553720
Radoslaw Choluj <radekcholuj@wp.pl>
2021-01-10 21:36:33 +0100

    #5 implementacja zmiennych, dodanie pojazdów do salonu, wyswietlenie marki modelu i ceny podanych pojazdów

---
+++ src/Salon.java
@@ -0,0 +1,12 @@
+public class Salon {
+    char[] mark;
+    char[] model;
+    double price;
+
+    Salon(char[] mark, char[] model, double price){
+        this.mark = mark;
+        this.model = model;
+        this.price = price;
+    }
+}
+
